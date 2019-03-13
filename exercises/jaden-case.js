f301db2898a277fac6b7764454cf5d117fbc1b74
Anisse Mahtat <anissemahtat@yahoo.com>
2019-03-13 15:24:58 +0100

    f

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -9,11 +9,19 @@
  * Example : "How are you ?" -> "How Are You ?"
  *
  */
-
+function jadenCase (string) {
+    let array = [];
+    let words = string.split(' ');
+    for ( let i = 0 ; i < words.length ; i++) {
+        let word = words [i] ;
+        array.push(word[0].toUpperCase()+ word.slice(1))
+    }
+    return array.join(' ')
+}
 
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.equal(jadenCase('deux trois mots'),'Deux Trois Mots')
 // End of tests */
